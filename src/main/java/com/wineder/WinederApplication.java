2b76e40389a9ee0812cacf136667ef8e73a3febc
lim-jaein <limjaein00@gmail.com>
2025-07-30 10:08:15 +0900

    initial commit

---
+++ src/main/java/com/wineder/WinederApplication.java
@@ -0,0 +1,13 @@
+package com.wineder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinederApplication.class, args);
+	}
+
+}
